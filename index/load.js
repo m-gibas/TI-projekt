cb238c488747d4302f3ac70e82c139ae3cb2d8cd
Maksymilian Gibas <104219943+m-gibas@users.noreply.github.com>
2022-12-12 21:48:47 +0100

    main page

---
+++ index/load.js
@@ -0,0 +1,26 @@
+var myLink = document.getElementById("mylink");
+let iterations = 0;
+
+myLink.onclick = function () {
+  if (iterations === 0) {
+    oblicz();
+
+    setTimeout(() => {
+      script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = "canvas.js";
+      document.getElementsByTagName("head")[0].appendChild(script);
+    }, 50);
+
+    iterations++;
+  } else {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    window.cancelAnimationFrame(myReq);
+    i = 0;
+    oblicz();
+    canvas.width = T[T.length - 1] * 105;
+    canvas.height = H[0] * 120;
+  }
+
+  return false;
+};
